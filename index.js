ec1b2ae1386625c2a9ac2f5dc6e94210f49e6f6a
Melissa <melissa.wanjiku@student.moringaschool.com>
2023-12-16 15:26:50 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+
+var customerName = 'bob';
+
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(value) {
+  bestCustomer = value;
+}
+
+
+const leastFavoriteCustomer = 'Susan';
+
+function changeLeastFavoriteCustomer() {
+ 
+  leastFavoriteCustomer = 'Bob';
+}
